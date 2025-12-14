7a4692098a3e042d56d7c45a9a6d22b503f76c1c
Kasshan Khan <kasshankhan@hotmail.com>
2025-12-15 01:06:22 +0530

    UI change

--- padhAI_frontend/src/signup.jsx
+++ padhAI_frontend/src/signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './LoginSignup.css';
+import bankuLogo from './assets/banku_logo.jpg';
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -44,68 +45,79 @@ const Signup = () => {
   };
 
   return (
-    <div className='loginsignup-container'>
-      <div className="loginsignup-header">
-        <div className="loginsignup-text">Sign Up</div>
-        <div className="loginsignup-underline"></div>
+    <div className="loginsignup-outer-container">
+      <div className = "banku">
+        <img src={bankuLogo} alt="banku" border="0" className='bankuLogo'/>
       </div>
-
-      {/* IMPORTANT: onSubmit */}
-      <form onSubmit={handleSubmit}>
-        <div className="loginsignup-inputs">
-
-          <div className="loginsignup-input">
-            <input
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+      <div className = "banku">
+        <div className='loginsignup-container'>
+          <div className="loginsignup-header">
+            <div className="loginsignup-text">  
+              <p    className = "text_ab_ab_sngup">Welcome to padhAI</p>
+              <p className = "text_ab_sngup">Let's get started</p>
+            </div>
+            <div className="loginsignup-underline"></div>
           </div>
 
-          <div className="loginsignup-input">
-            <input
-              type="email"
-              placeholder="Email Id"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+          {/* IMPORTANT: onSubmit */}
+          <form onSubmit={handleSubmit}>
+            <div className="loginsignup-inputs">
 
-          <div className="loginsignup-input">
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+              <div className="loginsignup-input">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
 
-          <div className="loginsignup-input">
-            <select
-              value={goal}
-              onChange={(e) => setGoal(e.target.value)}
-            >
-              <option value="" disabled>Select Goal</option>
-              <option value="JEE">JEE</option>
-              <option value="NEET">NEET</option>
-            </select>
-          </div>
+              <div className="loginsignup-input">
+                <input
+                  type="email"
+                  placeholder="Email Id"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
 
+              <div className="loginsignup-input">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <div className="loginsignup-input">
+                <select
+                  value={goal}
+                  onChange={(e) => setGoal(e.target.value)}
+                >
+                  <option value="" disabled>Select Goal</option>
+                  <option value="JEE">JEE</option>
+                  <option value="NEET">NEET</option>
+                  <option value="ENGG.">Engineering</option>
+                </select>
+              </div>
+
+            </div>
+
+            <div className="loginsignup-submit-container">
+              <button type="submit" className="loginsignup-submit">
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          <div className="loginsignup-toggle">
+            <p>
+              Already have an account?
+              <Link to="/login"><span> Login here</span></Link>
+            </p>
+          </div>
         </div>
-
-        <div className="loginsignup-submit-container">
-          <button type="submit" className="loginsignup-submit">
-            Sign Up
-          </button>
-        </div>
-      </form>
-
-      <div className="loginsignup-toggle">
-        <p>
-          Already have an account?
-          <Link to="/login"><span> Login here</span></Link>
-        </p>
       </div>
     </div>
   );
